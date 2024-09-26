6d2b0bc5abb6006cf95f6bf3a99ceadcb71f7efc
Mandy2114 <mandydecamp1213@gmail.com>
2024-09-26 12:16:20 -0400

    add env connect MONGODB

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
+const dotenv = require("dotenv")
+dotenv.config();
+
+
 // Here is where we import modules
 // We begin by loading Express
 const express = require("express");
+const mongoose = require("mongoose")
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+mongoose.connect(process.env.MONGODB_URL);
+
+mongoose.connection.on("connected", () => {
+  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
